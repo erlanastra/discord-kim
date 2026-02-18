c694c1cd1837b631f812402db9e9ba6f6a8c12c0
erlanastra <erlanastra1@gmail.com>
2026-02-18 23:15:47 +0800

    Update command bot

--- cogs/announce.py
+++ cogs/announce.py
@@ -8,7 +8,7 @@ class Announcement(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    @commands.has_any_role("🛡️ | Mod DC", "🎖️ | Mod YT", "🧭 | Pembina OSIS", "📝| OSIS")
+    @commands.has_any_role("🛡️| Mod DC", "🎖️ | Mod YT", "🧭 | Pembina OSIS", "📝| OSIS")
     async def announce(self, ctx, *, pesan):
         """Kirim announcement dengan embed menarik"""
         embed = discord.Embed(
